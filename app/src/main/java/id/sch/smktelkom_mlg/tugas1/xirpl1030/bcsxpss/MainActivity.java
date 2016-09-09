dfd4e9b984ee28f0464b6b1d7852f2bddf4328a3
Nugrahanto <rianirsyahnugrahanto@gmail.com>
2016-09-09 18:41:22 +0700

    #SecondCommit +Nama, Tahun, Pendidikan (java)

--- app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl1030/bcsxpss/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl1030/bcsxpss/MainActivity.java
@@ -1,13 +1,129 @@
 package id.sch.smktelkom_mlg.tugas1.xirpl1030.bcsxpss;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+
+	EditText etNama, etTahun;
+	Spinner spProvinsi, spKota;
+	CheckBox cbSMA, cbMHS, cbKRJ;
+	Button bOk;
+	TextView tvHasil1, tvHasil2, tvPendidikan;
+	int nHobi;
+
+	String[][]arKota = {{"Jakarta Barat", "Jakarta Pusat", "Jakarta Selatan", "Jakarta Timur", "Jakarta Utara"},
+			{"Bandung", "Cirebon", "Bekasi"},{"Semarang", "Magelang", "Surakarta"},
+			{"Yogyakarta", "Sleman", "Bantul", "Kulon Progo", "Gunung Kidul" },
+			{"Surabaya", "Malang",},{"Denpasar"}};
+	ArrayList<String> listKota = new ArrayList<>();
+	ArrayAdapter<String> adapter;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
+		etNama     	= (EditText) findViewById(R.id.Nama);
+		etTahun    	= (EditText) findViewById(R.id.Tahun);
+		spProvinsi 	= (Spinner) findViewById(R.id.spinnerProvinsi);
+		spKota     	= (Spinner) findViewById(R.id.spinnerKota);
+		cbSMA 		= (CheckBox) findViewById(R.id.checkBoxSMA);
+		cbMHS 		= (CheckBox) findViewById(R.id.checkBoxMHS);
+		cbKRJ 	  	= (CheckBox) findViewById(R.id.checkBoxKRJ);
+		bOk        	= (Button) findViewById(R.id.buttonOK);
+
+		cbSMA.setOnCheckedChangeListener(this);
+		cbMHS.setOnCheckedChangeListener(this);
+		cbKRJ.setOnCheckedChangeListener(this);
+
+		tvHasil1 = (TextView) findViewById(R.id.textViewHasil1);
+		tvHasil2 = (TextView) findViewById(R.id.textViewHasil2);
+		tvPendidikan  = (TextView) findViewById(R.id.textViewPendidikan);
+
+		adapter    = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listKota);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spKota.setAdapter(adapter);
+
+		spProvinsi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+				listKota.clear();
+				listKota.addAll(Arrays.asList(arKota[pos]));
+				adapter.notifyDataSetChanged();
+				spKota.setSelection(0);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> adapterView) {
+
+			}
+		});
+
+		findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				doClick();
+			}
+		});
+
+	}
+
+	private void doClick() {
+		String nama = etNama.getText().toString();
+		String tahun = etTahun.getText().toString();
+		String hasil2 = "Pendidikan anda : " ;
+		int startlen = hasil2.length();
+
+		if (nama.isEmpty()) {
+			etNama.setError("Nama belum diisi");
+		}
+		else if (nama.length() <= 3) {
+			etNama.setError("Nama minimal 3 karakter");
+		}
+		else {
+			etNama.setError(null);
+		}
+
+		if (tahun.isEmpty()) {
+			etTahun.setError("Tanggal Lahir belum diiisi");
+		}
+		else if (tahun.length() != 10) {
+			etTahun.setError("Format Tanggal Lahir Salah");
+		}
+		else {
+			etTahun.setError(null);
+		}
+		tvHasil1.setText("Nama                   : "+nama + "\nTanggal Lahir      : "+tahun);
+
+
+		if (cbSMA.isChecked()) hasil2+=cbSMA.getText()+", ";
+		if (cbMHS.isChecked()) hasil2+=cbMHS.getText()+", ";
+		if (cbKRJ.isChecked()) hasil2+=cbKRJ.getText()+". ";
+
+		if (hasil2.length()==startlen) hasil2+="Tidak ada pada Pilihan";
+		tvHasil2.setText(hasil2);
+	}
+
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		if(isChecked) nHobi+=1;
+		else nHobi-=1;
+
+		tvPendidikan.setText("Pendidikan");
 	}
 }
